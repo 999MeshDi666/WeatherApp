3b3c68c320d7b56d443570c8f80956d027bc43f2
madi <havkpzz@gmail.com>
2023-10-03 14:51:00 +0600

    Styled intro screen

--- src/screen/IntroScreen.jsx
+++ src/screen/IntroScreen.jsx
@@ -1,18 +1,47 @@
-import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image, FlatList } from 'react-native';
 import { global } from '../../assets/styles/global';
 import Container from '../components/Container';
+
+const images = [
+    {
+        src: require("../../assets/images/weather-images/sun.png"),
+        class: "imgSun"
+    },
+    {
+        src: require("../../assets/images/weather-images/cloud.png"),
+        class: "imgCloud1"
+    },
+    {
+        src: require("../../assets/images/weather-images/cloud.png"),
+        class: "imgCloud2"
+    },
+    {
+        src: require("../../assets/images/weather-images/cloud.png"),
+        class: "imgCloud3"
+    }
+]
+
 const IntroScreen = ({navigation}) =>{
     return(
         <Container>
-            <View>
-                <Text style={[global.text, styles.title]}>
-                    {`Your 
-                      Weather 
-                      App`}
-                </Text>
-                <Text style={[global.text, styles.subtitle]}>
-                    All weather forecast information in one app
-                </Text>
+            <View style={{flex: 1}}>
+                <View style={{flex: 1}}>
+                    {images.map((img, index)=>(
+                        <Image
+                            key={index}
+                            style={[{position: "absolute"}, styles[img.class]]} 
+                            source={img.src}
+                        />
+                    ))}
+                </View>
+                <View >
+                    <Text style={[global.text, styles.title]}>
+                        Your Weather App
+                    </Text>
+                    <Text style={[global.text, styles.subtitle]}>
+                        All weather forecast information in one app
+                    </Text>
+                </View>
             </View>
             <TouchableOpacity 
                 style={styles.buttonContainer} 
@@ -27,18 +56,36 @@ const IntroScreen = ({navigation}) =>{
 }
 
 const styles = StyleSheet.create({
-    
     title: {
         fontSize: 45,
         fontFamily: "Montserrat-ExtraBold",
-        lineHeight: 20
+        width: 250
     },
     subtitle: {
+        marginTop: 20,
         fontSize: 16,
+        fontFamily: "Montserrat-Medium",
+        width: 300
+    },
+    imgSun: {
+        top: 40, 
+        left: 120
+    },
+    imgCloud1: {
+        top: -32, 
+        left: 40
+    },
+    imgCloud2: {
+        top: 5,  
+        right: 45
+    },
+    imgCloud3: {
+        top: 100, 
+        left: 50
     },
     buttonContainer:{
-        height: 56,
-        marginTop: 50,
+        height: 60,
+        marginVertical: 50,
         paddingVertical: 10,
         borderColor: "#000",
         borderWidth: 1,
@@ -47,7 +94,7 @@ const styles = StyleSheet.create({
     },
     buttonText:{
         textAlign: "center",
-        fontWeight: "bold",
+        fontFamily: "Montserrat-Bold",
         fontSize: 24
     }
 })
